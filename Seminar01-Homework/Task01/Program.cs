95a6a9f03bf02c3f0e2fb908becd915c23f734c5
Alex Ko <help.mail015@gmail.com>
2022-04-18 00:00:28 +0300

    Add Seminar01 comments and flowchart

--- Seminar01-Homework/Task01/Program.cs
+++ Seminar01-Homework/Task01/Program.cs
@@ -1,4 +1,5 @@
-﻿int a = -9;
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = -9;
 int b = -3;
 if (a > b) Console.WriteLine("max = " + a + ", min = " + b);
 else Console.WriteLine("max = " + b + ", min = " + a);
