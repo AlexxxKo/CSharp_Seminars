a499a34d26261a78e9a239018311ee85997a83df
Alex Ko <help.mail015@gmail.com>
2022-05-05 00:23:16 +0300

    change seminar 01 task 01

--- Seminar01-Homework/Task01/Program.cs
+++ Seminar01-Homework/Task01/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-int a = -9;
-int b = -3;
-if (a > b) Console.WriteLine("max = " + a + ", min = " + b);
-else Console.WriteLine("max = " + b + ", min = " + a);
+double a = new Random().NextDouble();
+double b = new Random().NextDouble();
+if (a > b) Console.WriteLine($"max = {a}, min = {b}");
+else Console.WriteLine($"max = {b}, min = {a}");
