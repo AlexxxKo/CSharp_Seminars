873b44f1d545008e8e1cdfa0701c5c95cc4b0a68
Alex Ko <help.mail015@gmail.com>
2022-05-05 01:58:06 +0300

    change seminar 01 task 03

--- Seminar01-Homework/Task03/Program.cs
+++ Seminar01-Homework/Task03/Program.cs
@@ -1,5 +1,15 @@
-﻿// 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-int a = 7;
+﻿// 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+int GetValue(string text)
+{
+	Console.Write($"{text}: ");
+	string getValueStr = Console.ReadLine()!;
+	int value = Convert.ToInt32(getValueStr);
+	return value;
+}
+
+int a = GetValue("a");
+
 if (a % 2 == 0)
 {
 	Console.WriteLine("Чётное");
