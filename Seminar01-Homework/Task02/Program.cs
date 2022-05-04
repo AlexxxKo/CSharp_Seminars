f81c31076db4c581c273261b725500cf4cebc9e2
Alex Ko <help.mail015@gmail.com>
2022-05-05 01:29:59 +0300

    change seminar 01 task 02

--- Seminar01-Homework/Task02/Program.cs
+++ Seminar01-Homework/Task02/Program.cs
@@ -1,9 +1,19 @@
-﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-int a = 42;
-int b = 3;
-int c = 7;
+﻿// 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-int max = a;
+double GetValue(string text)
+{
+	Console.Write($"{text}: ");
+	string getValueStr = Console.ReadLine()!;
+	double value = Convert.ToDouble(getValueStr);
+	return value;
+}
+
+Console.Clear();
+double a = GetValue("a");
+double b = GetValue("b");
+double c = GetValue("c");
+
+double max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
