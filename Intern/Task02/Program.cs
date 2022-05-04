7067f38ee90cb8894d629462ef7f4c90950e07d2
Alex Ko <help.mail015@gmail.com>
2022-05-05 00:22:07 +0300

    add intern task02

---
+++ Intern/Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. По двум заданным числам проверять является ли первое квадратом второго
+
+double GetValue(string text)
+{
+	Console.Write($"{text}: ");
+	string getValueStr = Console.ReadLine()!;
+	double value = Convert.ToDouble(getValueStr);
+	return value;
+}
+
+Console.Clear();
+double a = GetValue("a");
+double b = GetValue("b");
+
+if (a == b * b) Console.WriteLine("Yes");
+else Console.WriteLine("No");
