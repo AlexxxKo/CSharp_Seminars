4be8548ef8a7b133576ca2abebee3ddc062b9ca7
Alex Ko <help.mail015@gmail.com>
2022-05-05 02:13:39 +0300

    add changes

--- Seminar01-Homework/Task04/Program.cs
+++ Seminar01-Homework/Task04/Program.cs
@@ -1,5 +1,15 @@
-﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-int n = 1;
+﻿// 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int GetValue(string text)
+{
+	Console.Write($"{text}: ");
+	string getValueStr = Console.ReadLine()!;
+	int value = Convert.ToInt32(getValueStr);
+	return value;
+}
+
+int n = GetValue("n");
+
 if (n > 0)
 {
 	int number = 2;
--- Seminar01-Homework/Task05/Program.cs
+++ Seminar01-Homework/Task05/Program.cs
@@ -1,5 +1,13 @@
-﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-int n = 4, m;
+﻿// 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+int GetValue(string text)
+{
+	Console.Write($"{text}: ");
+	string getValueStr = Console.ReadLine()!;
+	int value = Convert.ToInt32(getValueStr);
+	return value;
+}
+int n = GetValue("n"), m;
 
 if (n > 0)
 {
