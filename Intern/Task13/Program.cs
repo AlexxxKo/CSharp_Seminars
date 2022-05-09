1caad04c8a3990c97f38bee52cf6fa36604b3e6f
Alex Ko <help.mail015@gmail.com>
2022-05-10 01:00:59 +0300

    finish intern

---
+++ Intern/Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿// 13. Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+
+int a = new Random().Next(100, 1000);
+// Console.WriteLine(a);
+
+a = a / 100 * 10 + a % 10;
+
+Console.WriteLine(a);
